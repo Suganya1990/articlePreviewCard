0bc1bd227b8742c3f6393556d99d678f372f59d8
Suganya Maheswaran <suganyamaheswaran@gmail.com>
2020-11-11 10:27:06 -0500

    updated mobile css

--- main.js
+++ main.js
@@ -3,15 +3,15 @@ const share = document.querySelector('.article__share');
 const author = document.querySelector('.article__avatar');
 const authorInfo = document.querySelector('.article__author');
 const shareHover = document.querySelector('.article__share--hover')
-const shareIcon = document.querySelector('.share-icon')
-console.log(shareIcon);
 
 
 
 share.onmouseover = () =>{
     author.style.display = 'none';
     authorInfo.style.display = 'none';
-    shareHover.style.display = 'flex'
+    shareHover.style.display = 'flex';
+    share.style.background = "hsl(214, 17%, 51%)"
+    share.style.fill = "white"
   
 
 }
