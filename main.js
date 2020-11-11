f908d5ac18da53dfcc65e9a907038a15d101fdbf
Suganya Maheswaran <suganyamaheswaran@gmail.com>
2020-11-11 09:23:52 -0500

    css update

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const share = document.querySelector('.article__share');
 const author = document.querySelector('.article__avatar');
 const authorInfo = document.querySelector('.article__author');
 const shareHover = document.querySelector('.article__share--hover')
-
+const shareIcon = document.querySelector('.share-icon')
+console.log(shareIcon);
 
 
 
@@ -11,6 +12,7 @@ share.onmouseover = () =>{
     author.style.display = 'none';
     authorInfo.style.display = 'none';
     shareHover.style.display = 'flex'
+  
 
 }
 
