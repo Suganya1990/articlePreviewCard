304c513096dc240e1c84f0249a5ede5535190a41
Suganya Maheswaran <suganyamaheswaran@gmail.com>
2020-11-12 17:25:42 -0500

    updated js file

--- main.js
+++ main.js
@@ -3,21 +3,44 @@ const share = document.querySelector('.article__share');
 const author = document.querySelector('.article__avatar');
 const authorInfo = document.querySelector('.article__author');
 const shareHover = document.querySelector('.article__share--hover')
+var w ;
+
+
+function getSize(){ w = document.documentElement.clientWidth; console.log(w)}
 
 
 
 share.onmouseover = () =>{
+    
+    
+ if (w < 1028)   {
     author.style.display = 'none';
     authorInfo.style.display = 'none';
     shareHover.style.display = 'flex';
     share.style.background = "hsl(214, 17%, 51%)"
     share.style.fill = "white"
-  
+
+ }
+ else{
+    author.style.display = 'block';
+    authorInfo.style.display = 'block';
+    shareHover.style.display = 'block'
+ }
 
 }
 
 share.onmouseout = ()=>{
-    author.style.display = 'block';
-    authorInfo.style.display = 'block';
-    shareHover.style.display = 'none'
+    if (w < 1028)   {
+        author.style.display = 'block';
+        authorInfo.style.display = 'block';
+        shareHover.style.display = 'none'
+    }
+    else{
+        author.style.display = 'block';
+        authorInfo.style.display = 'block';
+        shareHover.style.display = 'none';
+        share.style.background = "hsl(214, 17%, 51%)"
+        share.style.fill = "white"
+    
+    }
 }
